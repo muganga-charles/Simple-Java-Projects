c5f9f43b97ad3133cca504bab6d2cf7f3d7ad0dc
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-31 10:40:36 +0300

    Add files via upload

---
+++ pollingStation/PoliticalParty.java
@@ -0,0 +1,14 @@
+public class PoliticalParty {
+
+    String partyName;
+
+    public PoliticalParty(String partyName) {
+        this.partyName = partyName;
+    }
+    public String getPartyName() {
+        return partyName;
+    }
+    public void setPartyName(String partyName) {
+        this.partyName = partyName;
+    }
+}
